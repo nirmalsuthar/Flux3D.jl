255c37bc077a11c5289f759ca3bb84d54e994068
nirmalsuthar <61233297+nirmalsuthar@users.noreply.github.com>
2020-04-20 10:26:10 +0530

    Update visualize.jl

--- src/visualize.jl
+++ src/visualize.jl
@@ -4,7 +4,7 @@ function visualize(v::PointCloud; kwargs...)
     get!(kwargs, "markersize") do
         0.02*v.npoints/1024
     end
-    get(kwargs, "color") do
+    get!(kwargs, "color") do
         :blue
     end
     Makie.AbstractPlotting.meshscatter(v.points[:,1],v.points[:,2],v.points[:,3], kwargs...)
